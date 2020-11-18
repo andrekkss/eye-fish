020044dc934fe4e27a9c450c7cd2c7fe8cc93647
André Kitano <andre.kitano.silva@gmail.com>
2020-11-18 20:38:25 -0300

    fixed packages

--- src/features/monitor/page/monitor.js
+++ src/features/monitor/page/monitor.js
@@ -5,19 +5,6 @@ import { config, dataConfig } from '../platform/monitor';
 import Typography from '@material-ui/core/Typography';
 import "chartjs-plugin-streaming";
 
-const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');
-const tty = "/dev/ttyACM0"; 
-//O tty vario na rasp-berry para localizo:
-//ls /dev/tty*
-const port = new SerialPort(tty, { baudRate: 9600 });
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
-
-// Read the port data
-port.on("open", () => {
-  console.log('serial port open');
-});
-
 const useStyles = makeStyles(() => ({
     root: {
         width: '100% !important',
@@ -37,20 +24,20 @@ export default function Monitor(){
     const [options, setOptions] = useState(config(refresh));
     
     function refresh(){
-      parser.on('data', data =>{
-        const ph = parseFloat(data)
-        setPh(ph);
-        data.datasets[0].data.push({
-            x: Date.now(),
-            y: ph
-        });
+      // parser.on('data', data =>{
+      //   const ph = parseFloat(data)
+      //   setPh(ph);
+      //   data.datasets[0].data.push({
+      //       x: Date.now(),
+      //       y: ph
+      //   });
+      // });
+      const ph = randomFloat(0,10)
+      setPh(ph);
+      data.datasets[0].data.push({
+         x: Date.now(),
+         y: ph
       });
-      //const ph = randomFloat(0,10)
-      //setPh(ph);
-      //data.datasets[0].data.push({
-      //    x: Date.now(),
-      //    y: ph
-      //});
     }
 
     const classes = useStyles();
